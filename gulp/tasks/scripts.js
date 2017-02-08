4b34bc26c3c1a2655a6a6733670308c4947d1c87
matteobertoldo <mrbert93@gmail.com>
2017-02-08 15:10:05 +0100

    Add dinamic jQuery + Modernizr Build

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -7,42 +7,89 @@
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var conf = require('../gulpconfig');
+var file = require('gulp-file');
+var modernizr = require('modernizr');
+var modernizrProp = require('modernizr/lib/config-all.json');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
-var uglifyJS2 = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 var rename = require('gulp-rename');
+var sequence = require('run-sequence');
 
-// @global scripts sources
+// @build: global jquery lib
 // -----------------------
 
-var src = [conf.workspace.vendor + '**/*.js', (conf.packageManager.manage) ? conf.packageManager.src : [], conf.workspace.main + '**/*.js'];
-var arr = [];
+gulp.task('jquery', function() {
+    return gulp.src(conf.jsOptions.jQuery)
+    .pipe(plumber())
+    .pipe(gulpif(conf.jsOptions.minifyjQuery, uglify({
+        compress: true
+    }))).on('error', gutil.log)
+    .pipe(gulpif(conf.jsOptions.minifyjQuery, rename({
+        suffix: '.min'
+    })))
+    .pipe(gulp.dest(conf.distribution.lib));
+});
 
-// @convert sources into a single array (no sub array)
+// @build: modernizr lib
+// -----------------------
+
+var prefix = (conf.jsOptions.modernizrCustomOptions) ? '.custom.js' : '.js';
+var prop = (conf.jsOptions.modernizrCustomOptions) ? conf.jsOptions.customizr : modernizrProp;
+
+gulp.task('modernizr', function() {
+    return modernizr.build(prop, function(result) {
+        file('modernizr' + prefix, result, {
+            src: false
+        })
+        .pipe(plumber())
+        .pipe(gulpif(conf.jsOptions.minifyModernizr, uglify({
+            compress: true
+        }))).on('error', gutil.log)
+        .pipe(gulpif(conf.jsOptions.minifyModernizr, rename({
+            suffix: '.min'
+        })))
+        .pipe(gulp.dest(conf.distribution.lib));
+    });
+});
+
+// @global: plugins sources
+// @concat: convert sources into a single array (no sub array)
+// @build: "bundle" scripts (vendors + main)
 // @note: `run-sequence` don't accept a "sub array".
 // -----------------------
+
+var src = [conf.workspace.vendor + '**/*.js', (conf.packageManager.managePlugins) ? conf.packageManager.src : [], conf.workspace.main + '**/*.js'];
+var arr = [];
 
 for (var i = 0; i < src.length; i++) {
     arr = arr.concat(src[i]);
 }
 
-gulp.task('scripts', function() {
+gulp.task('bundle', function() {
     return gulp.src(arr)
     .pipe(plumber())
-	.pipe(gulpif(conf.jsOptions.minify, sourcemaps.init()))
-    .pipe(concat(conf.jsOptions.outputName + '.js'))
-    .pipe(gulpif(conf.jsOptions.minify, uglifyJS2({
+	.pipe(gulpif(conf.jsOptions.minifyPlugins, sourcemaps.init()))
+    .pipe(concat(conf.jsOptions.outputPluginsName + '.js'))
+    .pipe(gulpif(conf.jsOptions.minifyPlugins, uglify({
         compress: true
     }))).on('error', gutil.log)
-    .pipe(gulpif(conf.jsOptions.minify, rename({
+    .pipe(gulpif(conf.jsOptions.minifyPlugins, rename({
         suffix: '.min'
     })))
-    .pipe(gulpif(conf.jsOptions.minify, sourcemaps.write('./')))
+    .pipe(gulpif(conf.jsOptions.minifyPlugins, sourcemaps.write('./')))
     .pipe(gulp.dest(conf.distribution.js));
 });
 
-gulp.task('scripts:watch', ['scripts'], function() {
+gulp.task('bundle:watch', ['bundle'], function() {
     return global.browserSync.reload();
+});
+
+// @build: all libraries
+// -----------------------
+
+gulp.task('libs', function(done) {
+    sequence('jquery', 'modernizr', done);
 });
